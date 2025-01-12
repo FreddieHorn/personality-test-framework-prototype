0fdd5651f72da8812f6982bbe8c7edb9043d62d0
FreddieHorn <fryderykrog7@gmail.com>
2025-01-12 14:33:42 +0100

    udpated agent's prompts and ocde

--- environment.py
+++ environment.py
@@ -1,22 +1,54 @@
 
 class Agent:
-    def __init__(self, personality, scenario:str, goal: str, start = False):
+    def __init__(self, personality, scenario:str, goal: str, json_schema:dict, start = False):
         self.scenario = scenario
         self.personality = personality # How is it represented?
         self.goal = goal
-        self.agent_engine = self._set_personality()
+        self._set_agent(start)
 
-    def _set_personality(self):
-        # Initialization based on config - LLM 
-        return None
-    
-    def take_turn(self, incoming_message = None):
-        if not incoming_message:
-            pre_message = "you will begin the conversation with the other agent in order to achieve your goal in the given scenario"
-            output_message = self.agent_engine(pre_message+)
-        else:
-            output_message = self.agent_engine(incoming_message) # LLM processes utterance from the other agent and responds
-        return output_message
+    def _set_agent(self, start):
+        if start:
+            starting_msg = f"You will begin the conversation. Your character will be marked as agent_1 in the conversation. "
+        else: 
+            starting_msg = f"Your character will be marked as agent_2 in the conversation."
+
+        self.system_message = f"""
+        ### Instructions: ###
+        You will roleplay as a popular character from (e.g.) a movie, pop-culture, politics in a specific scenario in order to achieve a given goal. You will try to behave as your given character as good as you can.
+        Pay attention to personality traits of the character. Especially take into consideration the Big5 personality traits i.e. Openness, Conscientiousness, Agreeableness, Extroversion, Neuroticism.
+        When roleplaying as a character, try to achieve your goal, but most importantly stick to the character's personality. Behave and act like a character. Always be aware of how the character would act in every situation.
+        You will interact with another character in a given scenario in a form of conversation. You will try to achieve the goal using verbal communication. You will receive the conversation between you and the other agent and reply with the next response.
+        The character name: {character_name}. 
+        ### Scenario: ###
+        {scenario}
+        ### Character's goal: ###
+        {goal}
+        ### Starting msg: ###
+        {starting_msg}
+        """
+        self.chat_template = """### System:
+        {system_message}
+        ### User:
+        {user_message}
+        """
+
+    def take_turn(self, conversation: list):
+        json_format = {"response" : "Your character's next statement in the conversation"}
+        user_message = f"""
+        What's do you say next in the conversation (your turn). 
+        ### The conversation: ###
+        {conversation}
+        ### Format: ###
+        use a simple format
+        """
+        messages = [
+            {"role": "system", "content": self.system_message},
+            {"role": "user", "content": user_message},
+        ]
+        prompt = tokenizer.apply_chat_template(messages, chat_template=chat_template, tokenize=False, add_generation_prompt=True, return_tensors="pt")
+        jsonformer = Jsonformer(self.model, self.tokenizer, self.json_schema, prompt)
+        generated_data = jsonformer()
+        return generated_data["response"]
 
 # Class that emulates interaction between agents (agentic behaviour)
 class AgentInteraction:
@@ -38,11 +70,10 @@ class AgentInteraction:
                 )
     def conduct_interaction(self):
         episode = []
-        last_utterance = None
         for i in range(turns):
-            last_utterance = agent_1.take_turn(last_utterance)
+            last_utterance = agent_1.take_turn(episode)
             episode.append(f"agent_1: '{last_utterance}'")
-            last_utterance = agent_2.take_turn(last_utterance)
+            last_utterance = agent_2.take_turn(episode)
             episode.append(f"agent_2: '{last_utterance}'")
         return episode
     
