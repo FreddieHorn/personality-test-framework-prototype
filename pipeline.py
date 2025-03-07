70003620d200a78bc0690ae78f75c03835a7c9a5
FreddieHorn <fryderykrog7@gmail.com>
2025-03-07 15:50:40 +0100

    working pipeline

--- pipeline.py
+++ pipeline.py
@@ -8,7 +8,9 @@ from interaction_generation import generate_interaction
 from evaluation import evaluation
 
 if __name__ == "__main__":
-    
+    tempratures = [1,2,3,4,5]
+    interaction_modes = ["default", "script"]
+    characters_csvs = ["baseline_trump.csv", "baseline_oprah.csv", "baseline_warren.csv", "baseline_zuck.csv"]
     MODEL_NAME = "meta-llama/Meta-Llama-3.1-8B-Instruct"
     # Get the current date and time formatted as YYYY-MM-DD_HH-MM
     folder_name = datetime.now().strftime("%Y-%m-%d_%H-%M")
@@ -34,6 +36,16 @@ if __name__ == "__main__":
     tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
     print("models declared")
-    scenario_generation(step_0_csv_path, step_1_csv_path, model, tokenizer, temperature = 5)
-    generate_interaction(step_1_csv_path, step_2_csv_path, model, tokenizer)
-    evaluation(step_2_csv_path, step_3_csv_path, model, tokenizer)
+    for temperature in tempratures: # I know that this triple for loop is diabolical and bash script probably would be better
+        for inter_mode in interaction_modes:
+            for csv in characters_csvs:
+                print(f"PROCESSING {csv} || interaction_mode: {inter_mode} || temperature {temperature}")
+                folder_name = f"{csv.split("_")[1].split(".")[0]}_{inter_mode}_{temperature}"
+                os.makedirs(f"output/{folder_name}", exist_ok=True)
+                step_0_csv_path = csv
+                step_1_csv_path = f'output/{folder_name}/scenarios.csv'
+                step_2_csv_path = f'output/{folder_name}/interactions.csv'
+                step_3_csv_path = f'output/{folder_name}/evaluated_interactions.csv'
+                scenario_generation(step_0_csv_path, step_1_csv_path, model, tokenizer, temperature = temperature)
+                generate_interaction(step_1_csv_path, step_2_csv_path, model, tokenizer, mode=inter_mode)
+                evaluation(step_2_csv_path, step_3_csv_path, model, tokenizer)
